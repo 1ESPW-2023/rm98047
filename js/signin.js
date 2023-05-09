eea305212676a2bbb0cba640ba8e87749f6c109f
luigiferrarasinno <rm98047@fiap.com>
2023-05-09 20:49:10 -0300

    ultimo commit da aula

--- js/signin.js
+++ js/signin.js
@@ -2,19 +2,54 @@
 
 //CRIANDO UM OBJETO
 const usuario1 = {
-    
-} 
+    nomeUsuario:"pt001",
+    senhaUsuario:"12345"
+}
+
+const usuario2 = {
+    nomeUsuario:"pt002",
+    senhaUsuario:"12345"
+}
+
+//CRIAR UMA LISTA DE OBJETOS
+let listaDeUsuarios = [];
+listaDeUsuarios.push(usuario1);
+listaDeUsuarios.push(usuario2);
+
+//Adicionando a lista de OBJETOS no localStorage
+localStorage.setItem("listaUser", JSON.stringify(listaDeUsuarios));
 
 addEventListener("click", (evt)=>{
     if(evt.target.id == "btnSubmit"){
 
         let usuarioInput = document.querySelector("#usuario").value;
         let senhaInput = document.querySelector("#senha").value;
+        const msgError = document.querySelector("#msgError");
 
-        if(usuarioInput == "pf0670" && senhaInput == "12345"){
-            console.log("USUÁRIO VALIDADO!");
-        }else{
-            console.log("SENHA OU NOME DE USUÁRIO INVÁLIDO!");
-        }   
+        try{
+
+            let listaUser = JSON.parse(localStorage.getItem("listaUser"));
+
+            listaUser.forEach((usuario)=>{
+
+                if(usuarioInput == usuario.nomeUsuario && senhaInput == usuario.senhaUsuario){
+                    throw "USUÁRIO VALIDADO!";
+                }
+            });
+            throw "SENHA OU NOME DE USUÁRIO INVÁLIDO!";
+        }catch(err){
+            if(err == "USUÁRIO VALIDADO!"){
+                
+                msgError.setAttribute("style","color:#00ff00;display:block;")
+                msgError.innerHTML = "<strong>Usuário validado!</strong>"
+
+            }else{
+                msgError.setAttribute("style","color:#ff0000;display:block;");
+                msgError.innerHTML = "<strong>Senha ou nome de Usuário inválidos!</strong>";
+            }
+        }
+           
+
+
     }
 });
